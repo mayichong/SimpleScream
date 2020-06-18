cfb3a7eba047360ce4de9a0f9237cd8723ee2fc2
Yichong Ma <mayichong123@gmail.com>
2020-06-18 14:51:22 -0400

    first commit

---
+++ firebase/functions/util/fbAuth.js
@@ -0,0 +1,41 @@
+//import both admin and db from admin.js
+const { admin, db } = require("./admin");
+
+//middleware
+module.exports = (req, res, next) => {
+  let idToken;
+  //check if we have a token or not
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  //if we have token, continue
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      //assign handle and imageUrl locally, export for other component to use
+      req.user.handle = data.docs[0].data().handle;
+      req.user.imageUrl = data.docs[0].data().imageUrl;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
+};
---
+++ firebase/functions/util/validators.js
@@ -0,0 +1,79 @@
+//check if a field is empty or not
+const isEmpty = (string) => {
+  if (string.trim() === "") {
+    return true;
+  } else return false;
+};
+
+//check if input is a valid email form or not
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) {
+    return true;
+  } else return false;
+};
+
+//start to check sign up data
+exports.validateSignupData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
+
+  if (data.password !== data.confirmPassword)
+    errors.confirmPassword = "Passwords must match";
+
+  if (isEmpty(data.handle)) errors.handle = "Must not be empty";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+//start to validate login data
+exports.validateLoginData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = "Must not be empty";
+  }
+
+  if (isEmpty(data.password)) {
+    errors.password = "Must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+//start to validate user details data
+exports.reduceUserDetails = (data) => {
+  let userDetails = {};
+  console.log("data is:");
+  console.log(data);
+  if (!isEmpty(data.bio.trim())) {
+    userDetails.bio = data.bio;
+  }
+
+  if (!isEmpty(data.website.trim())) {
+    if (data.website.trim().substring(0, 4) !== "http") {
+      userDetails.website = `http://${data.website.trim()}`;
+    } else {
+      console.log(userDetails);
+      userDetails.website = data.website;
+    }
+  }
+  if (!isEmpty(data.location.trim())) {
+    userDetails.location = data.location;
+  }
+
+  return userDetails;
+};
